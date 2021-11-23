8e06a55651d6203cf8b3ccc26bafc20ce7cae89f
Anthony Nash <anthony.nash@du.co>
2021-11-23 14:26:30 +0000

    Split out service watches, use annotations for persistence

--- src/controller.py
+++ src/controller.py
@@ -1,30 +1,16 @@
 import kopf
 
-from kopf import Logger, Meta, Spec, Status, Annotations
 
-from const import KOPF_LB_PARAMS
-from utils import get_svc
-from loadbalancer import LoadBalancer
-
-
-@kopf.on.create('services', **KOPF_LB_PARAMS)
-@kopf.on.resume('services', **KOPF_LB_PARAMS)
-def create_lb(logger: Logger, meta: Meta, spec: Spec, status: Status, annotations: Annotations, **_):
-    logger.info(f"Adding LoadBalancer {meta.namespace}/{meta.name}")
-    lb = LoadBalancer(
-        ip=status["loadBalancer"]["ingress"][0]["ip"],
-        annotations=annotations,
-        svc=get_svc()
+@kopf.on.startup()
+def configure(settings: kopf.OperatorSettings, memo: kopf.Memo, **_):
+    settings.persistence.finalizer = "advertise.upnp/kopf-finalizer"
+    settings.persistence.progress_storage = kopf.AnnotationsProgressStorage(
+        prefix="advertise.upnp"
     )
-    print(lb.json())
+    settings.persistence.diffbase_storage = kopf.AnnotationsDiffBaseStorage(
+        prefix="advertise.upnp",
+        key="last-handled-configuration",
+    )
 
 
-@kopf.on.update('services', **KOPF_LB_PARAMS)
-def update_lb(logger: Logger, meta: Meta, old: Spec, new: Status, **_):
-    logger.info(f"Updating LoadBalancer {meta.namespace}/{meta.name}")
-
-
-@kopf.on.delete('services', **KOPF_LB_PARAMS)
-def delete_lb(logger: Logger, meta: Meta, spec: Spec, status: Status, **_):
-    logger.info(f"Deleting LoadBalancer {meta.namespace}/{meta.name}")
-
+import services
