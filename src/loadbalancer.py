d29b2fa7a0d30083daf0abbebd12fcbd915e4948
Anthony Nash <anthony.nash@du.co>
2022-02-15 21:05:07 +0000

    LB advertise/unadvertise

--- src/loadbalancer.py
+++ src/loadbalancer.py
@@ -1,7 +1,8 @@
+from ipaddress import ip_address
 from pydantic import BaseModel
-from typing import List, Union
+from typing import Tuple, List, Union
 from upnpy.ssdp.SSDPDevice import SSDPDevice
-from kopf import Annotations
+from kopf import Annotations, Logger
 
 from utils import get_ports
 
@@ -10,13 +11,41 @@ class LoadBalancer(BaseModel):
     ip: str
     svc: SSDPDevice.Service
     annotations: Annotations
-    tcp_whitelist: Union[List[int], None]
-    udp_whitelist: Union[List[int], None]
+    ports: List[Tuple[str,int]]
 
     class Config:
         arbitrary_types_allowed = True
 
     def __init__(self, **data):
-        data["tcp_whitelist"] = get_ports("tcp", data["annotations"])
-        data["udp_whitelist"] = get_ports("udp", data["annotations"])
+        data["ports"] = []
+        for proto in ["tcp", "udp"]:
+            ports = get_ports(proto, data["annotations"])
+            ports = [(proto.upper(), port) for port in ports]
+            data["ports"].extend(ports)
         super().__init__(**data)
+
+    def advertise(self, logger: Union[Logger, None]) -> None:
+        for proto, port in self.ports:
+            if logger is not None:
+                logger.info(f"Adding port mapping for {self.ip}:{port}/{proto}")
+            self.svc.AddPortMapping(
+                NewRemoteHost="",
+                NewExternalPort=port,
+                NewProtocol=proto,
+                NewInternalPort=port,
+                NewInternalHost=self.ip,
+                NewEnabled=1,
+                NewPortMappingDescription="",
+                NewLeaseDuration=0
+            )
+
+    def unadvertise(self, logger: Union[Logger, None]):
+        for proto, port in self.ports:
+            if logger is not None:
+                logger.info(f"Deleting port mapping for {self.ip}:{port}/{proto}")
+
+            self.svc.DeletePortMapping(
+                NewRemoteHost="",
+                NewExternalPort=port,
+                NewProtocol=proto,
+            )
