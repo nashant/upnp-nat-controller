15df3d76839c7d72fd622252bd138e8ebe061a27
Anthony Nash <anthony.nash@du.co>
2022-02-15 21:05:28 +0000

    Sort out create/update/delete

--- src/services.py
+++ src/services.py
@@ -7,6 +7,13 @@ from utils import get_svc
 from loadbalancer import LoadBalancer
 
 
+def get_ip(d: dict, status: Status):
+    try:
+        return d["status"]["loadBalancer"]["ingress"][0]["ip"]
+    except (KeyError, IndexError):
+        return status["loadBalancer"]["ingress"][0]["ip"]
+
+
 @kopf.on.create('services', **KOPF_LB_PARAMS)
 @kopf.on.resume('services', **KOPF_LB_PARAMS)
 def create_lb(logger: Logger, meta: Meta, spec: Spec, status: Status, annotations: Annotations, **_):
@@ -16,14 +23,33 @@ def create_lb(logger: Logger, meta: Meta, spec: Spec, status: Status, annotation
         annotations=annotations,
         svc=get_svc()
     )
-    logger.info(lb.dict())
+    lb.advertise(logger)
 
 
 @kopf.on.update('services', **KOPF_LB_PARAMS)
-def update_lb(logger: Logger, meta: Meta, old: Spec, new: Status, **_):
+def update_lb(logger: Logger, meta: Meta, old: Spec, new: Spec, annotations: Annotations, status: Status, **_):
     logger.info(f"Updating LoadBalancer {meta.namespace}/{meta.name}")
+    old_lb = LoadBalancer(
+        ip=get_ip(old, status),
+        annotations=old.get("spec", {}).get("annotations", annotations),
+        svc=get_svc()
+    )
+    new_lb = LoadBalancer(
+        ip=get_ip(new, status),
+        annotations=new.get("spec", {}).get("annotations", annotations),
+        svc=get_svc()
+    )
+    old_lb.unadvertise(logger)
+    new_lb.advertise(logger)
+
 
 
 @kopf.on.delete('services', **KOPF_LB_PARAMS)
-def delete_lb(logger: Logger, meta: Meta, spec: Spec, status: Status, **_):
+def delete_lb(logger: Logger, meta: Meta, spec: Spec, annotations: Annotations, status: Status, **_):
     logger.info(f"Deleting LoadBalancer {meta.namespace}/{meta.name}")
+    lb = LoadBalancer(
+        ip=status["loadBalancer"]["ingress"][0]["ip"],
+        annotations=annotations,
+        svc=get_svc()
+    )
+    lb.unadvertise(logger)
